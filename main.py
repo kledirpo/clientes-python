47f6a306c62f082dc359e6d00603b12e0dd02939
Kledir Oliveira <kledir@protonmail.com>
2022-07-16 13:11:18 -0300

    Atualização do arquivo principal
    Estruturado o código em funções para a divisão de tarefas.

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 #View - o que vai aparecer para o usuario
-usuario = input("Digite o nome de usuario: \n")
-senha = input("Digite sua senha: \n")
+def view():
+    usuario = input("Digite o nome de usuario: \n")
+    senha = input("Digite sua senha: \n")
+    controller(usuario, senha)
 
 
-#Model - Oque vem do banco de dados
-usuario_DB = 'joao'
-senha_DB = '123'
+#Model - O que vem do banco de dados
+def model_usuario():
+    usuario_DB = 'joao'
+    return usuario_DB
+
+def model_senha():
+    senha_DB = '123'
+    return senha_DB
 
 # Controller - A Validação entre o View e o Model.
-if usuario == usuario_DB and senha == senha_DB:
-    print("Pode Acessar")
-else:
-    print("Usuario ou Senha inválido!")
+def controller(usuario,senha):
+   usuario_DB = model_usuario() 
+   senha_DB = model_senha() 
+   if usuario == usuario_DB and senha == senha_DB:
+        print("Pode Acessar")
+   else:
+        print("Usuario ou Senha inválido!")
+
+
+# Chama a função principal que irá aparecer para o usuario
+view()
