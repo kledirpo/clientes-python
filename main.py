7e289ec41186055a2c80f7f7c5129b609b5e050c
Kledir Oliveira <kledir@protonmail.com>
2022-07-23 11:48:20 -0300

    nova estrutura do repositorio
    commit inicial

---
+++ main.py
@@ -0,0 +1,3 @@
+import controllers.validacao as controller
+
+controller.iniciar()
---
+++ models/banco.py
@@ -0,0 +1,18 @@
+# Model - O que vem do banco de dados (BD)
+
+def model_usuario():
+    arquivo = open("models\\dados.txt", "r+")
+    conteudo = arquivo.readlines()
+    for linha in conteudo:
+        usuario_senha = linha.split(";")
+    usuario_BD = usuario_senha[0]
+    return usuario_BD
+
+def model_senha():
+    arquivo = open("models\\dados.txt", "r+")
+    conteudo = arquivo.readlines()
+    for linha in conteudo:
+        usuario_senha = linha.split(";")
+    senha_BD = usuario_senha[1]
+    return senha_BD
+
